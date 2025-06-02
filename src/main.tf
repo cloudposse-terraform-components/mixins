4ba6a040308764761c48e14f89a12c237836ed70
Benjamin Smith <ben.smith.developer@gmail.com>
2025-06-02 09:39:25 -0700

    initial copy from cloudposse/terraform-aws-components

--- src/main.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  enabled = module.this.enabled
-}
-
-
-
-
-
--- src/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "mock" {
-  description = "Mock output example for the Cloud Posse Terraform component template"
-  value       = local.enabled ? "hello ${basename(abspath(path.module))}" : ""
-}
-
-
-
