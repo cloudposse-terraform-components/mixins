cd12d30acf46b51dfbd1b8b5da93e9e1719e53c3
Igor Rodionov <496956+goruha@users.noreply.github.com>
2025-11-20 15:04:07 +0300

    Add optional fields to account_map variable

--- src/mixins/provider-without-account-map.tf
+++ src/mixins/provider-without-account-map.tf
@@ -8,15 +8,21 @@ variable "account_map_enabled" {
 
 variable "account_map" {
   type = object({
-    full_account_map           = map(string)
-    audit_account_account_name = optional(string, "")
-    root_account_account_name  = optional(string, "")
+    full_account_map              = map(string)
+    audit_account_account_name    = optional(string, "")
+    root_account_account_name     = optional(string, "")
+    identity_account_account_name = optional(string, "")
+    aws_partition                  = optional(string, "aws")
+    iam_role_arn_templates         = optional(map(string), {})
   })
   description = "Map of account names (tenant-stage format) to account IDs. Used to verify we're targeting the correct AWS account. Optional attributes support component-specific functionality (e.g., audit_account_account_name for cloudtrail, root_account_account_name for aws-sso)."
   default = {
-    full_account_map           = {}
-    audit_account_account_name = ""
-    root_account_account_name  = ""
+    full_account_map              = {}
+    audit_account_account_name    = ""
+    root_account_account_name     = ""
+    identity_account_account_name = ""
+    aws_partition                 = "aws"
+    iam_role_arn_templates        = {}
   }
 }
 
